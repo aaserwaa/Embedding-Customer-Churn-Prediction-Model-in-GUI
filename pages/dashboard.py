b74cbf584a533a8271eadc5f11e156f971c9b3e6
aaserwaa <amaserwaaakoto399@gmail.com>
2024-03-06 19:31:44 +0000

    add dashboard.py file

---
+++ pages/dashboard.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def show_dashboard_page():
+    st.write("Dashboard page is under construction.")
+    
+        # Call the data function directly
+if __name__ == '__main__':
+    show_dashboard_page()
