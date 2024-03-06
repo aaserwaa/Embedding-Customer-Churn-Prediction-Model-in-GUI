3ca5968f5806b98fefc65e0e740453ab1318da08
aaserwaa <amaserwaaakoto399@gmail.com>
2024-03-06 19:32:28 +0000

    add history.py file

---
+++ pages/history.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def show_history_page():
+    st.write("History page is under construction.")
+    
+        # Call the data function directly
+if __name__ == '__main__':
+    show_history_page()
